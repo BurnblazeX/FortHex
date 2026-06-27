6ae90a0d000a711b415445abcf88bf9960bc13f8
BurnblazeX <64275367+BurnblazeX@users.noreply.github.com>
2026-06-27 18:07:31 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -55,7 +55,7 @@ const THEME = {
 };
         
         // --- Game Configuration & Constants ---
-        const BUILD_VERSION = "Indev B29";
+        const BUILD_VERSION = "B29";
         const HEX_SIZE = 70; 
         const CANVAS_WIDTH_NORMAL = (2 * 3 + 1.5) * (HEX_SIZE * Math.sqrt(3));
         const CANVAS_HEIGHT_NORMAL = (2 * 3 + 1) * (HEX_SIZE * 2 * 0.75) + HEX_SIZE;
